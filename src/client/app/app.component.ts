17315b183dc69366f6acee498afe58e640a9d655
Michael VanderHelm <mikevh@gmail.com>
2017-08-29 11:24:22 -0700

    Debugging node .ts files

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'angles-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angles';
+
+  constructor(private http: Http) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('/api').subscribe(x => {
+      this.title = x.json().message;
+    });
+  }
+
 }
